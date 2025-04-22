f22b1d19708937ca5890d9f226d291586d9cd30b
Haikiri <59623220+MKC-MKC@users.noreply.github.com>
2025-04-22 13:51:13 +0300

    Update Update.php

--- src/Entity/Update.php
+++ src/Entity/Update.php
@@ -185,14 +185,14 @@ class Update
 		foreach (
 			(function (): Generator {
 				yield $this->getChannelPost();
-				yield $this->getEditedChannelPost();
-				yield $this->getBusinessMessage();
-				yield $this->getEditedBusinessMessage();
+//				yield $this->getEditedChannelPost();
+//				yield $this->getBusinessMessage();
+//				yield $this->getEditedBusinessMessage();
 //				yield $this->getDeletedBusinessMessages();
 				yield $this->getMessage();
 //				yield $this->getMessageReaction();
 //				yield $this->getMessageReactionCount();
-				yield $this->getCallbackQuery()?->getMessage();
+//				yield $this->getCallbackQuery()?->getMessage();
 //				yield $this->getMyChatMember();
 //				yield $this->getChatMember();
 //				yield $this->getChatJoinRequest();
