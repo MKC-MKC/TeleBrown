69eeef9fe48f94454dbc6dc9416eb4fdd17c8e65
Haikiri <59623220+MKC-MKC@users.noreply.github.com>
2025-05-13 11:16:07 +0300

    попытка 2

--- src/TeleBrownServer.php
+++ src/TeleBrownServer.php
@@ -14,6 +14,13 @@ class TeleBrownServer extends TeleBrownServerAbstract
 	public string $proxy_user = "";
 	public string $proxy_pass = "";
 
+	public function __construct(string $url, string $token, $debug = false)
+	{
+		$this->setUrl($url !== "" ? $url : $this->url);
+		$this->setToken($token);
+		self::$debug = $debug;
+	}
+
 	/**
 	 * Метод отправки POST запроса на сервер API.
 	 *
