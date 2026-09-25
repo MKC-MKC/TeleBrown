89bd7f1619bf750ec6a6c3038044b8104ea82ed4
RAZIEL KOSTELLO <rkostello@haikiri.ru>
2026-09-25 17:09:50 +0300

    Реализовал изменение названия чата

--- src/Methods/ChatSettings.php
+++ src/Methods/ChatSettings.php
@@ -30,4 +30,28 @@ trait ChatSettings
 		)->isSuccess();
 	}
 
+	/**
+	 * Изменяем название чата. Названия личных чатов изменить нельзя.
+	 * Бот должен быть администратором с соответствующими правами.
+	 *
+	 * @param int|string $chatId ID чата, например -1001234567890, или имя @username.
+	 * @param string $title Новое название чата, от 1 до 128 символов.
+	 * @return bool true при успешном выполнении.
+	 * @throws TelegramMainException
+	 * @see https://core.telegram.org/bots/api#setchattitle
+	 */
+	public function setChatTitle(
+		int|string $chatId,
+		string     $title,
+	): bool
+	{
+		return $this->sendRequest(
+			method: __FUNCTION__,
+			params: [
+				"chat_id" => $chatId,
+				"title" => $title,
+			],
+		)->isSuccess();
+	}
+
 }
