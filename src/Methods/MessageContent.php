65b84126857f617c82f235e3eb8986bfe29f0f3f
RAZIEL KOSTELLO <rkostello@haikiri.ru>
2026-09-25 17:12:28 +0300

    Добавил метод sendVenue

--- src/Methods/MessageContent.php
+++ src/Methods/MessageContent.php
@@ -67,4 +67,84 @@ trait MessageContent
 		return new Objects\Message($response->getData());
 	}
 
+	/**
+	 * Отправляет информацию о месте.
+	 * Возвращается отправленное сообщение Message.
+	 *
+	 * @param int|string $chatId Идентификатор целевого чата или его @username.
+	 * @param float $latitude Широта места.
+	 * @param float $longitude Долгота места.
+	 * @param string $title Название места.
+	 * @param string $address Адрес места.
+	 * @param string|null $businessConnectionId Идентификатор бизнес-подключения, от имени которого отправляется сообщение.
+	 * @param int|null $messageThreadId Идентификатор темы форума; для супергрупп и личных чатов ботов с включённым режимом тем.
+	 * @param int|null $directMessagesTopicId Идентификатор темы личных сообщений канала; обязателен для чатов личных сообщений каналов.
+	 * @param Objects\EphemeralMessageParameters|null $ephemeralMessageParameters Параметры исчезающего сообщения.
+	 * @param string|null $foursquareId Идентификатор места в Foursquare.
+	 * @param string|null $foursquareType Тип места в Foursquare, например food/icecream.
+	 * @param string|null $googlePlaceId Идентификатор места в Google Places.
+	 * @param string|null $googlePlaceType Тип места в Google Places.
+	 * @param bool|null $disableNotification Отправка без звука уведомления.
+	 * @param bool|null $protectContent Защищает содержимое от пересылки и сохранения.
+	 * @param bool|null $allowPaidBroadcast Разрешает до 1000 сообщений в секунду за 0,1 звезды Telegram за сообщение.
+	 * @param string|null $messageEffectId Идентификатор эффекта сообщения; только для личных чатов.
+	 * @param Objects\SuggestedPostParameters|null $suggestedPostParameters Параметры предлагаемой публикации; только для чатов личных сообщений каналов.
+	 * @param Objects\ReplyParameters|null $replyParameters Описание сообщения, на которое отправляется ответ.
+	 * @param Objects\InlineKeyboardMarkup|Objects\ReplyKeyboardMarkup|Objects\ReplyKeyboardRemove|Objects\ForceReply|null $replyMarkup Inline-клавиатура, клавиатура ответа, её удаление или запрос ответа.
+	 * @return Objects\Message
+	 * @throws TelegramMainException
+	 * @see https://core.telegram.org/bots/api#sendvenue
+	 */
+	public function sendVenue(
+		int|string                                                                                                   $chatId,
+		float                                                                                                        $latitude,
+		float                                                                                                        $longitude,
+		string                                                                                                       $title,
+		string                                                                                                       $address,
+		string|null                                                                                                  $businessConnectionId = null,
+		int|null                                                                                                     $messageThreadId = null,
+		int|null                                                                                                     $directMessagesTopicId = null,
+		Objects\EphemeralMessageParameters|null                                                                      $ephemeralMessageParameters = null,
+		string|null                                                                                                  $foursquareId = null,
+		string|null                                                                                                  $foursquareType = null,
+		string|null                                                                                                  $googlePlaceId = null,
+		string|null                                                                                                  $googlePlaceType = null,
+		bool|null                                                                                                    $disableNotification = null,
+		bool|null                                                                                                    $protectContent = null,
+		bool|null                                                                                                    $allowPaidBroadcast = null,
+		string|null                                                                                                  $messageEffectId = null,
+		Objects\SuggestedPostParameters|null                                                                         $suggestedPostParameters = null,
+		Objects\ReplyParameters|null                                                                                 $replyParameters = null,
+		Objects\InlineKeyboardMarkup|Objects\ReplyKeyboardMarkup|Objects\ReplyKeyboardRemove|Objects\ForceReply|null $replyMarkup = null,
+	): Objects\Message
+	{
+		$response = $this->sendRequest(
+			method: __FUNCTION__,
+			params: [
+				"chat_id" => $chatId,
+				"latitude" => $latitude,
+				"longitude" => $longitude,
+				"title" => $title,
+				"address" => $address,
+				"business_connection_id" => $businessConnectionId,
+				"message_thread_id" => $messageThreadId,
+				"direct_messages_topic_id" => $directMessagesTopicId,
+				"ephemeral_message_parameters" => $ephemeralMessageParameters?->getAsArray(),
+				"foursquare_id" => $foursquareId,
+				"foursquare_type" => $foursquareType,
+				"google_place_id" => $googlePlaceId,
+				"google_place_type" => $googlePlaceType,
+				"disable_notification" => $disableNotification,
+				"protect_content" => $protectContent,
+				"allow_paid_broadcast" => $allowPaidBroadcast,
+				"message_effect_id" => $messageEffectId,
+				"suggested_post_parameters" => $suggestedPostParameters?->getAsArray(),
+				"reply_parameters" => $replyParameters?->getAsArray(),
+				"reply_markup" => $replyMarkup?->getAsArray(),
+			],
+		);
+
+		return new Objects\Message($response->getData());
+	}
+
 }
