202dd5b80394c54e77e7603b8b42cc0ef6a3a6f4
RAZIEL KOSTELLO <rkostello@haikiri.ru>
2026-09-25 17:27:57 +0300

    Реализовал удаление всех реакций участника

--- src/Methods/MessageReactions.php
+++ src/Methods/MessageReactions.php
@@ -41,4 +41,33 @@ trait MessageReactions
 		return $response->isSuccess();
 	}
 
+	/**
+	 * Удаляем до 10000 последних реакций пользователя или чата в группе или супергруппе.
+	 * Боту требуется право администратора can_delete_messages.
+	 *
+	 * @param int|string $chatId Идентификатор целевого чата или @username супергруппы.
+	 * @param int|null $userId Идентификатор пользователя, чьи реакции удаляются, если они добавлены пользователем.
+	 * @param int|null $actorChatId Идентификатор чата, чьи реакции удаляются, если они добавлены от имени чата.
+	 * @return bool true при успешном выполнении.
+	 * @throws TelegramMainException
+	 * @see https://core.telegram.org/bots/api#deleteallmessagereactions
+	 */
+	public function deleteAllMessageReactions(
+		int|string $chatId,
+		int|null   $userId = null,
+		int|null   $actorChatId = null,
+	): bool
+	{
+		$response = $this->sendRequest(
+			method: __FUNCTION__,
+			params: [
+				"chat_id" => $chatId,
+				"user_id" => $userId,
+				"actor_chat_id" => $actorChatId,
+			],
+		);
+
+		return $response->isSuccess();
+	}
+
 }
