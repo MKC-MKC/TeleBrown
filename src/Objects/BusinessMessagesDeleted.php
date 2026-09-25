4503b4cf514ebb35bf8df2acb6d5536758ed2d64
RAZIEL KOSTELLO <rkostello@haikiri.ru>
2026-09-25 16:52:29 +0300

    Реализовал модель удалённых бизнес-сообщений

--- src/Objects/BusinessMessagesDeleted.php
+++ src/Objects/BusinessMessagesDeleted.php
@@ -9,4 +9,38 @@ use Haikiri\TeleBrown\ResponseWrapper;
 class BusinessMessagesDeleted extends ResponseWrapper
 {
 
+	/**
+	 * Уникальный идентификатор бизнес-подключения.
+	 *
+	 * @return string
+	 * @see https://core.telegram.org/bots/api#businessmessagesdeleted
+	 */
+	public function getBusinessConnectionId(): string
+	{
+		return (string)$this->getData("business_connection_id");
+	}
+
+	/**
+	 * Чат бизнес-аккаунта. Бот может не иметь доступа к чату или соответствующему пользователю.
+	 *
+	 * @return Chat
+	 * @see https://core.telegram.org/bots/api#businessmessagesdeleted
+	 */
+	public function getChat(): Chat
+	{
+		$data = $this->getData("chat");
+		return new Chat($data);
+	}
+
+	/**
+	 * Идентификаторы удалённых сообщений в чате бизнес-аккаунта.
+	 *
+	 * @return array
+	 * @see https://core.telegram.org/bots/api#businessmessagesdeleted
+	 */
+	public function getMessageIds(): array
+	{
+		return (array)$this->getData("message_ids", []);
+	}
+
 }
