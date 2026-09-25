77648eb26d291748b67a83c3b670e467dc871c63
RAZIEL KOSTELLO <rkostello@haikiri.ru>
2026-09-25 17:12:28 +0300

    Добавил метод sendDice

---
+++ src/Methods/MessageContent.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haikiri\TeleBrown\Methods;
+
+use Haikiri\TeleBrown\Enums;
+use Haikiri\TeleBrown\Exceptions\TelegramMainException;
+use Haikiri\TeleBrown\Objects;
+
+trait MessageContent
+{
+
+	/**
+	 * Отправляет анимированный эмодзи со случайным значением.
+	 * Возвращается отправленное сообщение Message.
+	 *
+	 * @param int|string $chatId Идентификатор целевого чата или его @username.
+	 * @param string|null $businessConnectionId Идентификатор бизнес-подключения, от имени которого отправляется сообщение.
+	 * @param int|null $messageThreadId Идентификатор темы форума; для супергрупп и личных чатов ботов с включённым режимом тем.
+	 * @param int|null $directMessagesTopicId Идентификатор темы личных сообщений канала; обязателен для чатов личных сообщений каналов.
+	 * @param string|null $emoji Эмодзи анимации: 🎲, 🎯, 🏀, ⚽, 🎳 или 🎰. По умолчанию 🎲.
+	 * @param bool|null $disableNotification Отправка без звука уведомления.
+	 * @param bool|null $protectContent Защищает содержимое от пересылки и сохранения.
+	 * @param bool|null $allowPaidBroadcast Разрешает до 1000 сообщений в секунду за 0,1 звезды Telegram за сообщение.
+	 * @param string|null $messageEffectId Идентификатор эффекта сообщения; только для личных чатов.
+	 * @param Objects\SuggestedPostParameters|null $suggestedPostParameters Параметры предлагаемой публикации; только для чатов личных сообщений каналов.
+	 * @param Objects\ReplyParameters|null $replyParameters Описание сообщения, на которое отправляется ответ.
+	 * @param Objects\InlineKeyboardMarkup|Objects\ReplyKeyboardMarkup|Objects\ReplyKeyboardRemove|Objects\ForceReply|null $replyMarkup Inline-клавиатура, клавиатура ответа, её удаление или запрос ответа.
+	 * @return Objects\Message
+	 * @throws TelegramMainException
+	 * @see https://core.telegram.org/bots/api#senddice
+	 */
+	public function sendDice(
+		int|string                                                                                                   $chatId,
+		string|null                                                                                                  $businessConnectionId = null,
+		int|null                                                                                                     $messageThreadId = null,
+		int|null                                                                                                     $directMessagesTopicId = null,
+		string|null                                                                                                  $emoji = null,
+		bool|null                                                                                                    $disableNotification = null,
+		bool|null                                                                                                    $protectContent = null,
+		bool|null                                                                                                    $allowPaidBroadcast = null,
+		string|null                                                                                                  $messageEffectId = null,
+		Objects\SuggestedPostParameters|null                                                                         $suggestedPostParameters = null,
+		Objects\ReplyParameters|null                                                                                 $replyParameters = null,
+		Objects\InlineKeyboardMarkup|Objects\ReplyKeyboardMarkup|Objects\ReplyKeyboardRemove|Objects\ForceReply|null $replyMarkup = null,
+	): Objects\Message
+	{
+		$response = $this->sendRequest(
+			method: __FUNCTION__,
+			params: [
+				"chat_id" => $chatId,
+				"business_connection_id" => $businessConnectionId,
+				"message_thread_id" => $messageThreadId,
+				"direct_messages_topic_id" => $directMessagesTopicId,
+				"emoji" => $emoji,
+				"disable_notification" => $disableNotification,
+				"protect_content" => $protectContent,
+				"allow_paid_broadcast" => $allowPaidBroadcast,
+				"message_effect_id" => $messageEffectId,
+				"suggested_post_parameters" => $suggestedPostParameters?->getAsArray(),
+				"reply_parameters" => $replyParameters?->getAsArray(),
+				"reply_markup" => $replyMarkup?->getAsArray(),
+			],
+		);
+
+		return new Objects\Message($response->getData());
+	}
+
+}
--- src/TeleBrownServerAbstract.php
+++ src/TeleBrownServerAbstract.php
@@ -14,6 +14,7 @@ abstract class TeleBrownServerAbstract
 	use Methods\MessageEditing;
 	use Methods\LiveLocations;
 	use Methods\InlineQueries;
+	use Methods\MessageContent;
 	use Methods\MessageCopies;
 	use Methods\UserProfiles;
 	use Methods\ChatSettings;
