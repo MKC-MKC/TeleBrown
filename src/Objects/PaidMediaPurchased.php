81644cc9391ac9c5b4b8269dc04256bcd0fb5b70
RAZIEL KOSTELLO <rkostello@haikiri.ru>
2026-09-25 16:52:29 +0300

    Реализовал модель покупки платных медиа

--- src/Objects/PaidMediaPurchased.php
+++ src/Objects/PaidMediaPurchased.php
@@ -9,4 +9,27 @@ use Haikiri\TeleBrown\ResponseWrapper;
 class PaidMediaPurchased extends ResponseWrapper
 {
 
+	/**
+	 * Пользователь, купивший медиа.
+	 *
+	 * @return User
+	 * @see https://core.telegram.org/bots/api#paidmediapurchased
+	 */
+	public function getFrom(): User
+	{
+		$data = $this->getData("from");
+		return new User($data);
+	}
+
+	/**
+	 * Указанные ботом данные платного медиа.
+	 *
+	 * @return string
+	 * @see https://core.telegram.org/bots/api#paidmediapurchased
+	 */
+	public function getPaidMediaPayload(): string
+	{
+		return (string)$this->getData("paid_media_payload");
+	}
+
 }
